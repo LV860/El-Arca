605d5f3fcbffe176fc14d4b00e20209f7257ef31
Gustavo Ortiz <gustavo.ortiz7319@gmail.com>
2024-08-15 03:17:57 +0000

    Fin by id "Development"

--- demo/src/main/java/com/example/demo/controlador/MascotaController.java
+++ demo/src/main/java/com/example/demo/controlador/MascotaController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.servicio.MascotaService;
 
@@ -19,9 +22,15 @@ public class MascotaController {
         model.addAttribute("mascotas", mascotaService.SearchAll());
         return "html/tabla_Mascotas";
     }
+    @GetMapping("/find/{id}")
+    public String mostrarInfoMascota(Model model, @PathVariable("id") int identification){
+        model.addAttribute("mascotas", mascotaService.SearchById(identification));
+        return "html/mostrarMascotaPage";
+    }
     @GetMapping("/find")
-    public String mostrarInfoMascota(){
-        return "mostrar_mascotas";
+    public String mostrarInfoMascota2(Model model, @RequestParam("id") int identification){
+        model.addAttribute("mascotas", mascotaService.SearchById(identification));
+        return "html/mostrarMascotaPage";
     }
 
 
