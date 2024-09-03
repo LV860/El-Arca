4393635ad551ca8c3def4d2c2c7d8c69fdd3fce2
LV860 <plays860@gmail.com>
2024-09-02 22:30:18 -0500

    Mensaje error login

--- demo/src/main/java/com/example/demo/controlador/DuenhoController.java
+++ demo/src/main/java/com/example/demo/controlador/DuenhoController.java
@@ -22,17 +22,25 @@ public class DuenhoController {
 
     @PostMapping("/perfilCliente")
     public String mostrarPerfilCliente(@ModelAttribute Cliente cliente, Model model) {
-    
-        Cliente duenho = clienteService.findById(cliente.getCedula());
+        try {
+            Cliente duenho = clienteService.findById(cliente.getCedula());
 
-        if (duenho != null) {
-            model.addAttribute("cliente", duenho);
-            model.addAttribute("mascotas", duenho.getMascotas());
-            System.out.println("mascotas" + duenho.getMascotas().size());
-            System.out.println("mascotas" + duenho.getCedula());
-            return "/perfilCliente";
-        } else {
-            return "redirect:/home/loginCliente";
+            if (duenho != null) {
+                model.addAttribute("cliente", duenho);
+                model.addAttribute("mascotas", duenho.getMascotas());
+                System.out.println("mascotas: " + duenho.getMascotas().size());
+                System.out.println("cedula: " + duenho.getCedula());
+                return "/perfilCliente";
+            } else {
+                model.addAttribute("error", "No se encontró el cliente con la cédula proporcionada.");
+                return "/loginClienteError";
+            }
+        } catch (Exception e) {
+            // Manejar la excepción, por ejemplo, registrándola o mostrando un mensaje de error
+            System.err.println("Ocurrió un error: " + e.getMessage());
+            model.addAttribute("error", "Ocurrió un error al intentar mostrar el perfil del cliente.");
+            return "/loginClienteError";
         }
     }
+
 }
