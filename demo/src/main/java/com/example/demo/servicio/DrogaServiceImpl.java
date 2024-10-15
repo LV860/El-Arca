e7a7fbab19e5fba439f9f3f9342b303574c25563
LV860 <plays860@gmail.com>
2024-10-15 14:17:24 -0500

    servicio droga correcion

--- demo/src/main/java/com/example/demo/servicio/DrogaServiceImpl.java
+++ demo/src/main/java/com/example/demo/servicio/DrogaServiceImpl.java
@@ -3,10 +3,12 @@ package com.example.demo.servicio;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.entidad.Administrador;
 import com.example.demo.entidad.Droga;
 import com.example.demo.repositorio.DrogaRepository;
+@Service
 
 public class DrogaServiceImpl implements DrogaService {
    
