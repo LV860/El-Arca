30267b8678a6fd92b7176ce0126ed4cc90b05742
Thomas <thomasorbe2004@gmail.com>
2024-10-28 18:07:23 -0500

    Cliente Prueba Perfil Funciona

--- demo/src/test/java/com/example/demo/e2e/ClientePerfilTest.java
+++ demo/src/test/java/com/example/demo/e2e/ClientePerfilTest.java
@@ -2,11 +2,18 @@ package com.example.demo.e2e;
 
 import java.time.Duration;
 
-import org.junit.Test;
+import org.aspectj.lang.annotation.Before;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.internal.runners.statements.ExpectException;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
@@ -17,20 +24,20 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ActiveProfiles("test")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-public class ClientePerfilTest{
+public class ClientePerfilTest {
 
     private WebDriver driver;
     private WebDriverWait wait;
 
     @BeforeEach
-    public void init(){
+    public void init() {
         WebDriverManager.chromedriver().setup();
 
         ChromeOptions chromeOptions = new ChromeOptions();
 
         chromeOptions.addArguments("--disable-notifications");
         chromeOptions.addArguments("--disable-extensions");
-        //chromeOptions.addArguments("--headless");
+        // chromeOptions.addArguments("--headless");
 
         this.driver = new ChromeDriver(chromeOptions);
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));
@@ -38,10 +45,16 @@ public class ClientePerfilTest{
     }
 
     @Test
-    public void SystemTest_ClientePerfil_NombreCliente(){
+    public void SystemTest_perfilCliente_NombreCliente() {
 
-        driver.get("https://localhots:4200/perfilCliente/123456789");
+        driver.get("http://localhost:4200/perfilCliente/123456789");
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"nameSpan\"]")));
+        WebElement liNombre = driver.findElement(By.xpath("//*[@id=\"nameSpan\"]"));
+
+        // String expectedName = "Ana Pérez";
+        String expectedName = "";
+        Assertions.assertThat(liNombre.getText()).isEqualTo(expectedName);
     }
-
 
 }
