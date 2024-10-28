509dfbc3ec99fbcefc865df225f2785789e81d30
Thomas <thomasorbe2004@gmail.com>
2024-10-28 18:37:10 -0500

    ClientePerfilTest terminado

--- demo/src/test/java/com/example/demo/e2e/ClientePerfilTest.java
+++ demo/src/test/java/com/example/demo/e2e/ClientePerfilTest.java
@@ -1,11 +1,13 @@
 package com.example.demo.e2e;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.aspectj.lang.annotation.Before;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.internal.runners.statements.ExpectException;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -50,12 +52,42 @@ public class ClientePerfilTest {
 
         driver.get(BASE_URL + "/perfilCliente/123456789");
 
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"nameSpan\"]")));
-        WebElement liNombre = driver.findElement(By.xpath("//*[@id=\"nameSpan\"]"));
+        String pathNombre = "/html/body/app-root/app-perfil-cliente/main/div[1]/div/div[1]/div/span[1]";
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(pathNombre)));
+        WebElement liNombre = driver.findElement(By.xpath(pathNombre));
 
         // String expectedName = "Ana Pérez";
         String expectedName = "";
         Assertions.assertThat(liNombre.getText()).isEqualTo(expectedName);
     }
 
+    @Test
+    public void SystemTest_perfilCliente_NombreCliente2() {
+
+        driver.get(BASE_URL + "/perfilCliente/123456789");
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("nameSpan")));
+        WebElement liNombre = driver.findElement(By.id("nameSpan"));
+
+        // String expectedName = "Ana Pérez";
+        String expectedName = "";
+        Assertions.assertThat(liNombre.getText()).isEqualTo(expectedName);
+    }
+
+    @Test
+    public void SystemTest_perfilCliente_CantidadMascotas() {
+
+        driver.get(BASE_URL + "/perfilCliente/123456789");
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("NombreMascota")));
+        List<WebElement> liMascotas = driver.findElements(By.className("NombreMascota"));
+
+        Assertions.assertThat(liMascotas.size()).isEqualTo(2);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+    }
 }
